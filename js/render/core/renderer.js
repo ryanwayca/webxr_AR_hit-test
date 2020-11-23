ec6a6b00585cbdd2bd236b80bfae2c6b276671ca
Ryan Way <ryanway@Ryans-MacBook-Pro.local>
2020-11-23 12:26:28 -0500

    Reverted light color value

--- js/render/core/renderer.js
+++ js/render/core/renderer.js
@@ -45,7 +45,7 @@ export const ATTRIB_MASK = {
 const GL = WebGLRenderingContext; // For enums
 
 const DEF_LIGHT_DIR = new Float32Array([-0.1, -1.0, -0.2]);
-const DEF_LIGHT_COLOR = new Float32Array([13.0, 13.0, 13.0]);
+const DEF_LIGHT_COLOR = new Float32Array([3.0, 3.0, 3.0]);
 
 const PRECISION_REGEX = new RegExp('precision (lowp|mediump|highp) float;');
 
